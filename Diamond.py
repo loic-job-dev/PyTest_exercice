455d6b07298ade235795409a2edc626658b6f72d
loic-job-dev <loicjobard@hotmail.com>
2025-09-18 11:44:13 +0200

    bug loop

--- Diamond.py
+++ Diamond.py
@@ -4,17 +4,40 @@ alphabet = {letter: index for index, letter in enumerate(string.ascii_lowercase,
 def diamond(test) -> str:
     param = test.lower()
     index = alphabet[param]
-    referenceBoucle = 0
+    reference_boucle = 0
+    reference_alphabet = 0
     result = ""
 
-    while (referenceBoucle < index):
-        for letter in alphabet:
-            if letter == param:
-                result+=param
-                print(index)
-            else:
-                result+=" "
-        referenceBoucle+=1
+    while (reference_boucle < index*2-1):
+
+            for letter in alphabet:
+                if letter == param:
+                    result+=param
+                else:
+                    result+=" "
+            result+="\n"
+            reference_boucle+=1
+
+    return result
+
+print(diamond('d'))
+
+def diamond_boucle_ok(test) -> str:
+    param = test.lower()
+    index = alphabet[param]
+    reference_boucle = 0
+    reference_alphabet = 0
+    result = ""
+
+    while (reference_boucle < index*2-1):
+
+            while (reference_alphabet < index):
+                print(reference_alphabet)
+                reference_alphabet+=1
+
+            result+="\n"
+            reference_boucle+=1
+
     return result
 
 print(diamond('d'))
