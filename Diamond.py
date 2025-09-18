51171d3385b6598c5536009efed955c1d6b8868a
loic-job-dev <loicjobard@hotmail.com>
2025-09-18 13:58:21 +0200

    almost there

--- Diamond.py
+++ Diamond.py
@@ -9,9 +9,23 @@ def diamond(test) -> str:
     reference_alphabet = 1
     result = ""
 
-    while reference_boucle < index*2-1: #doit aller jusqu'à 7
+    #liste sur mesure pour boucler sur les lettres voulues seulement
+    letters = list(alphabet.keys())[:index]
+    reversed_letters = list(alphabet.keys())[:index]
+
+    reversed_letters.reverse()
+
+    print(letters)
+    while reference_boucle <= index*2-1:
         while reference_alphabet <= index: #Permet de boucler jusqu'à la lettre voulue
-            for letter in alphabet:
+
+            for letter in reversed_letters:
+                if letter == inverse_alphabet[reference_alphabet]:
+                    result+=letter
+                else:
+                    result+=" "
+
+            for letter in letters:
                 if letter == inverse_alphabet[reference_alphabet]:
                     result+=letter
                 else:
@@ -23,37 +37,24 @@ def diamond(test) -> str:
         reference_alphabet-=2
 
         while reference_alphabet > 0:
-            for letter in alphabet:
+            for letter in reversed_letters:
                 if letter == inverse_alphabet[reference_alphabet]:
                     result+=letter
                 else:
                     result+=" "
+
+            for letter in letters:
+                if letter == inverse_alphabet[reference_alphabet]:
+                    result+=letter
+                else:
+                    result+=" "
+
             reference_alphabet-=1
             result += "\n"
             reference_boucle+=1
 
     result+="fin de boucle"
+
     return result
 
 print(diamond('d'))
-
-
-
-
-def diamond_boucle_ok(test) -> str:
-    param = test.lower()
-    index = alphabet[param]
-    reference_boucle = 0
-    reference_alphabet = 0
-    result = ""
-
-    while (reference_boucle < index*2-1):
-
-            while (reference_alphabet < index):
-                print(reference_alphabet)
-                reference_alphabet+=1
-
-            result+="\n"
-            reference_boucle+=1
-
-    return result
