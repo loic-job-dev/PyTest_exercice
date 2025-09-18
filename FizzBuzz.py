74739d3e3ab7db27f1c1417778e73a4b9e1eee69
Léa Deffaisse <l.deffaisse@laposte.net>
2025-09-18 14:58:15 +0200

    fizzbuzz done

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,8 +1,14 @@
 def fizzbuzz(num: int) -> str:
     result = ""
     for i in range(1, num+1):
-        if i % 3 != 0:
-            result += f"{i}\n"
-        else:
+        if i % 3 == 0 and i % 5 == 0:
+            result += "fizzbuzz\n"
+        elif i % 3 == 0:
             result += "fizz\n"
+        elif i % 5 == 0:
+            result += "buzz\n"
+        else:
+            result += f"{i}\n"
     return result
+
+print(fizzbuzz(100))
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -4,5 +4,8 @@ from FizzBuzz import fizzbuzz
 
 def test_fizzbuzz_fizz():
     assert fizzbuzz(3) == "1\n2\nfizz\n"
-    assert fizzbuzz(5) == "1\n2\nfizz\n4\n5\n"
+    #assert fizzbuzz(5) == "1\n2\nfizz\n4\n5\n"
 
+def test_fizzbuzz_buzz():
+    assert fizzbuzz(5) == "1\n2\nfizz\n4\nbuzz\n"
+    assert fizzbuzz(15) == "1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizzbuzz\n"
