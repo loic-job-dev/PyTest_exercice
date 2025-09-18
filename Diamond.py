9813100b58721a705a3e281ab7274d6c6b3e5298
loic-job-dev <loicjobard@hotmail.com>
2025-09-18 12:35:19 +0200

    added forward loop

--- Diamond.py
+++ Diamond.py
@@ -9,29 +9,36 @@ def diamond(test) -> str:
     reference_alphabet = 1
     result = ""
 
-    while (reference_boucle < index*2-1):
-        while (reference_alphabet < index):
+    while reference_boucle < index*2-1: #doit aller jusqu'à 7
+        while reference_alphabet <= index: #Permet de boucler jusqu'à la lettre voulue
             for letter in alphabet:
                 if letter == inverse_alphabet[reference_alphabet]:
                     result+=letter
                 else:
                     result+=" "
             reference_alphabet+=1
+            result += "\n"
             reference_boucle+=1
 
-        for letter in alphabet:
-            if letter == inverse_alphabet[reference_alphabet]:
-                result+=param
-            else:
-                result+=" "
+        reference_alphabet-=2
+
+        while reference_alphabet > 0:
+            for letter in alphabet:
+                if letter == inverse_alphabet[reference_alphabet]:
+                    result+=letter
+                else:
+                    result+=" "
             reference_alphabet-=1
+            result += "\n"
             reference_boucle+=1
-        print(result)
 
-    result+= "\n"
+    result+="fin de boucle"
     return result
 
 print(diamond('d'))
+
+
+
 
 def diamond_boucle_ok(test) -> str:
     param = test.lower()
@@ -50,5 +57,3 @@ def diamond_boucle_ok(test) -> str:
             reference_boucle+=1
 
     return result
-
-print(diamond('d'))
