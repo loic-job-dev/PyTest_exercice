594c5e67b16727b6dd3174406629c1c784ae29bb
Léa Deffaisse <l.deffaisse@laposte.net>
2025-09-18 12:03:12 +0200

    added reverse alphabet

--- Diamond.py
+++ Diamond.py
@@ -1,23 +1,34 @@
 import string
 alphabet = {letter: index for index, letter in enumerate(string.ascii_lowercase, start=1)}
+inverse_alphabet = {index: letter for letter, index in alphabet.items()}
 
 def diamond(test) -> str:
     param = test.lower()
     index = alphabet[param]
-    reference_boucle = 0
-    reference_alphabet = 0
+    reference_boucle = 1
+    reference_alphabet = 1
     result = ""
 
     while (reference_boucle < index*2-1):
-
+        while (reference_alphabet < index):
             for letter in alphabet:
-                if letter == param:
-                    result+=param
+                if letter == inverse_alphabet[reference_alphabet]:
+                    result+=letter
                 else:
                     result+=" "
-            result+="\n"
+            reference_alphabet+=1
             reference_boucle+=1
 
+        for letter in alphabet:
+            if letter == inverse_alphabet[reference_alphabet]:
+                result+=param
+            else:
+                result+=" "
+            reference_alphabet-=1
+            reference_boucle+=1
+        print(result)
+
+    result+= "\n"
     return result
 
 print(diamond('d'))
