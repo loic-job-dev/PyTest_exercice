d7ca4c530defda4485140bbe9b7d416c3ed6815f
Léa Deffaisse <l.deffaisse@laposte.net>
2025-09-18 10:22:45 +0200

    test sub

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,5 @@
 from Calculator import add
+from Calculator import sub
 
 #To launch tests, execute 'pytest' command
 
@@ -7,3 +8,7 @@ def test_add():
     assert add(-2, -3) == -5
     assert add(3, -1) == 2
 
+def test_sub():
+    assert sub(2, 1) == 1
+    assert sub(-2, -3) == 1
+    assert sub(3, -1) == 4
